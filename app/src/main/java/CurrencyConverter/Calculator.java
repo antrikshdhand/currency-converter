636aaefa86c5074cbaeb39a516aa0f55671514c0
Nemo <nemogage@gmail.com>
2022-09-11 04:05:50 +1000

    Add Calculator class for currency conversion

---
+++ app/src/main/java/CurrencyConverter/Calculator.java
@@ -0,0 +1,11 @@
+package CurrencyConverter;
+
+public class Calculator {
+
+    private Database database;
+
+    public Calculator(Database database) {
+        this.database = database;
+    }
+
+}
